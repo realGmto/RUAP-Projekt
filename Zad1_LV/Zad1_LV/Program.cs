693bb99c0d1aab419b24a4ba7b2ffc786ebce193
Nikola Muženjak <gmto.gtmo@hotmail.com>
2023-10-31 17:51:42 +0100

    Update Zad1_LV

---
+++ Zad1_LV/Zad1_LV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zad1_LV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Nikole Muženjak \nIndeks: 12345");
+        }
+    }
+}
